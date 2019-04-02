6ca3f0847eed1c5fee0c9184c557b50d79867a9a
Alexandr <Alexandr@Home-PC>
2019-04-02 23:05:47 +0300

    Spring BOOT. Подключение БД MySQL.

---
+++ src/main/java/letscode/sarafan/domain/Views.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package letscode.sarafan.domain;
+
+/**
+ *
+ * @author Alexandr
+ */
+public final class Views {
+    public interface Id{}
+    
+    public interface IdName extends Id{}
+    
+    public interface FullMessage extends Id{}
+}
